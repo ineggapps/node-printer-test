b87773baff746dbee6985613bc4f013fe63d75a8
inegg <inegg.apps@gmail.com>
2020-04-02 10:02:30 +0900

    프린터 모듈 설치 후 테스트해보기..

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var printer = require("../lib");
+var util = require("util");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  const content =
+    "installed printers:\n" + util.inspect(printer.getPrinters(), { colors: true, depth: 10 });
+  res.send(content);
 });
 
 module.exports = router;
